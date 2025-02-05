8a49f87ddba3910bbdb0158511ade24b0ba2dcc6
sharjeel <148211409+Growup5@users.noreply.github.com>
2025-02-05 16:01:20 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Header/navbar';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
