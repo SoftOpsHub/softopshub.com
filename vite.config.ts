b8d970618551bbe622e1e02cd502df63f9142dc9
Growup5 <sharjeelshahid741@gmail.com>
2025-02-07 01:17:47 +0500

    update the package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    viteStaticCopy({
+      targets: [{ src: 'index.html', dest: '404.html' }]
+    })
+
   ],
 })
