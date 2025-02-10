131f2825601ba98b9cc694329d1f682419c41fda
Growup5 <sharjeelshahid741@gmail.com>
2025-02-10 18:06:00 +0500

    update the loader components

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './components/Header/navbar';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Service from './pages/Services';
-import Footer from './components/Footer/footer';
-import CloudDevops from './components/Service /CloudDevOps';
-import NotFound from './pages/NotFound'
-import Work from './pages/Work';
-import Team from './components/Team/Team';
+import Navbar from "./components/Header/navbar";
+import Footer from "./components/Footer/footer";
+import Loader from "./components/Loader/Loader";
+
+const Home = lazy(() => import("./pages/Home"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Service = lazy(() => import("./pages/Services"));
+const CloudDevops = lazy(() => import("./components/Service /CloudDevOps"));
+const Work = lazy(() => import("./pages/Work"));
+const Team = lazy(() => import("./components/Team/Team"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
 function App() {
   return (
     <Router>
       <Navbar />
       <div className="p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path ="/services" element={<Service />} />
-          <Route path ="/services/cloudDevops" element={<CloudDevops />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/team" element={<Team />} />
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/services" element={<Service />} />
+            <Route path="/services/cloudDevops" element={<CloudDevops />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </div>
       <Footer />
     </Router>
